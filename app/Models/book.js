0234bcf50aa53bea50b7caed82c09305197ae09e
Robinson Cook <robinsoncook@gmail.com>
2018-11-07 18:50:01 -0600

    Create And Get

--- app/Models/book.js
+++ app/Models/book.js
@@ -1,6 +1,12 @@
-//app/models/bear.js
+//app/models/Book.js
 
 //import mongoose
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//set default connection
+
+var BookSchema = new Schema({
+  name: String,
+  author: String
+})
+
+module.exports = mongoose.model('Book', BookSchema);
--- server.js
+++ server.js
@@ -5,26 +5,21 @@
 
 //package dependencies
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var app = express(); //app uses express
 
-//connections
+var db = mongoose.connect('mongodb://localhost:27017/books', function(error) {
+  if (error) console.log(error);
 
-mongoose.connect('mongodb://localhost/books');
-
-var connection = mongoose.connection;
-
-connection.on('connected', function(){
-  console.log('connected to db');
+  console.log("connection successful");
 });
-//Schema dependencies
+
 var Book = require('./app/models/book');
 
-
-
 //configure app to use bodyParser()
-//this will let us get the data from a repository
+//this will let us get the data from a POST
 app.use(bodyParser.urlencoded({
   extended: true
 })); //allows us to parse nested information instead of simple strings and arrays
@@ -36,12 +31,46 @@ var port = process.env.PORT || 8080; //sets the PORT
 //routes
 var router = express.Router(); //gets instance ouf express router
 
+//request middleware
+router.use(function(req, res, next) {
+  console.log('Something Happened');
+  next(); //make sure we go to the routes and dont get stuck
+});
+
 //test
-router.get('/',function(req,res){
-  res.json({message:'hooray! welcome to my api!'});
+router.get('/', function(req, res) {
+  res.json({
+    message: 'hooray! welcome to my api!'
+  });
 });
 
 //more routes
+
+//routes that end in /books
+router.route('/books')
+  .post(function(req, res) {
+    var book = new Book(); //create a new book object
+    book.name = req.body.name; //sets the name of the book(from the request)
+    book.author = req.body.author;
+    book.save(function(err) {
+      if (err) {
+        res.send(err)
+      }
+      res.json({ //responds with a json object
+        message: 'Book Created!'
+      })
+    })
+  })
+
+  .get(function(req, res) {
+    Book.find(function(err, books) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(books);
+    })
+  })
+
 //register routes
 app.use('/api', router);
 
